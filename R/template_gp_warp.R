e77f46c7f5ad80352315d0a87da18ad48e11cc99
wzhorton <wz.horton@gmail.com>
2018-10-05 13:27:35 -0600

    working GP model

--- R/template_gp_warp.R
+++ R/template_gp_warp.R
@@ -9,7 +9,8 @@
 #' @param template_feats a feature vector with length mathing those in feat_list
 #' @param niter,nburn MCMC iterations and burns. Total run time is the sum.
 #' @param int_p number of internal knots specified for the data curves
-#' @param asig,bsig,at,bt,al,bl,aa,ba hyperparameters for inverse gamma priors on model variances
+#' @param asig,bsig,at,bt,al,bl hyperparameters for inverse gamma priors on model variances
+#' @param aa,ba hyperparameters for the uniform prior on inverse range parameter
 #' @param progress logical; indicates whether or not to print percent progress.
 #' @return a list of warped curves, the estimated mean curve, and a vector of MCMC
 #'   acceptance rates. Curve list elements are vectors with length matching
@@ -19,7 +20,7 @@
 template_gp_warp <- function(y_list, feat_list, template_feats,
                              niter = 5000, nburn = 10000, int_p = 20,
                              asig = .1, bsig = .1, at = .1, bt = .1, al = .1, bl = .1,
-                             aa = .01, ba = .01, progress = TRUE){
+                             aa = .5, ba = 20, progress = TRUE){
 
   #----- Fixed Values -----#
 
@@ -30,10 +31,12 @@ template_gp_warp <- function(y_list, feat_list, template_feats,
 
   time <- seq(0, 1, len = m)
   y_vec <- unlist(y_list)
+  template_feats <- c(time[1], time[template_feats], time[m])
+  feat_list <- lapply(1:n, function(i) c(time[1], time[feat_list[[i]]], time[m]))
 
   p <- int_p + r
   knot_loc_p <- seq(time[1], time[m], len = int_p+2)[-c(1,int_p+2)]
-  Hp <- bs(time, knots = knot_loc_p, intercept = T)
+  Hp <- splines::bs(time, knots = knot_loc_p, intercept = T)
 
   P <- K1(p)
   P[1,1] <- 2
@@ -55,7 +58,7 @@ template_gp_warp <- function(y_list, feat_list, template_feats,
   lam2_save <- lapply(1:n, function(i) numeric(nrun))
   for(i in 1:n) lam2_save[[i]][1] <- 1
   alpha_save <- lapply(1:n, function(i) numeric(nrun))
-  for(i in 1:n) alpha_save[[i]][1] <- 50
+  for(i in 1:n) alpha_save[[i]][1] <- 1
 
 
   wtime <- lapply(1:n, function(i) time)
@@ -67,10 +70,12 @@ template_gp_warp <- function(y_list, feat_list, template_feats,
   H_list <- lapply(1:n, function(i) Hp)
   H_stack <- stack(H_list)$stack
 
-  R0 <- fields::rdist(time)
-  M_list <- lapply(1:n, function(i) fields::Matern(R0, alpha = 50, smoothness = 5.1))
+  create_M <- function(x_pts, y_pts = x_pts, alpha){
+    fields::Matern(fields::rdist(x_pts, y_pts), alpha = alpha, smoothness = 5.1)
+  }
 
-  mn_gp <- function(x) x
+  M_list <- lapply(1:n, function(i) create_M(x_pts = feat_list[[i]], alpha = 1))
+
 
   #----- MCMC Loop -----#
 
@@ -83,40 +88,43 @@ template_gp_warp <- function(y_list, feat_list, template_feats,
         cat("\b\b\b\b\b", round(it/nrun * 100), "%")
     }
 
-    #-- Update wtime, M, and alpha --#
+    #-- Update alpha, M --#
     for(i in 1:n){
-      current_llik <- dmnorm(y = y_list[[i]], mu =  H_list[[i]] %*% beta_save[it - 1,],
-                             prec = 1/ sig2_save[it - 1] * diag(m), log = TRUE)
-      current_lprior <- dinvgamma(x = alpha_save[[i]][it - 1], shape = aa, scale = ba, log = TRUE)
+      current_llik <- dmnorm(y = template_feats, mu = feat_list[[i]],
+                             cov = lam2_save[[i]][it - 1] * M_list[[i]], log = TRUE)
+      current_lprior <- dunif(alpha_save[[i]][it - 1], min = aa, max = ba, log = TRUE)
 
-      cand_alpha <- abs(rnorm(1, mean = alpha_save[[i]][it - 1], sd = tune))
-      cv_gp <- function(d) lam2_save[[i]][it - 1] * fields::Matern(d, alpha = cand_alpha, smoothness = 5.1)
+      cand_alpha <- rnorm(1, alpha_save[[i]][it - 1], tune)
+      cand_lprior <- dunif(cand_alpha, min = aa, max = ba, log = TRUE)
+      if(cand_lprior == -Inf){
+        alpha_save[[i]][it] <- alpha_save[[i]][it - 1]
+        next
+      }
+      cand_M <- create_M(x_pts = feat_list[[i]], alpha = cand_alpha)
+      cand_llik <- dmnorm(y = template_feats, mu = feat_list[[i]],
+                          cov = lam2_save[[i]][it - 1] * cand_M, log = TRUE)
 
-      cand_up <- update_gaussian_process(x = time[feat_list[[i]]], y = time[template_feats],
-                                         time = time, mnfun = mn_gp, covfun = cv_gp)
-      cand_wtime <- as.numeric(cand_up$up_mean)
-      cand_M <- cand_up$up_var
-      cand_H <- bs(cand_wtime, knots = knot_loc_p, intercept = TRUE)
-
-      cand_lprior <- dinvgamma(x = cand_alpha, shape = aa, scale = ba, log = TRUE)
-      cand_llik <- dmnorm(y = y_list[[i]], mu = cand_H %*% beta_save[it - 1,],
-                          prec = 1 / sig2_save[it - 1] * diag(m), log = TRUE)
-
-      if(log(runif(1)) < cand_llik + cand_lprior - current_llik - current_lprior){
-        accepts[i] <- accepts[i] + 1/nrun
+      lratio <- cand_llik + cand_lprior - current_llik - current_lprior
+      if(log(runif(1)) < lratio){
+        accepts[i] <- accepts[i] + 1
         alpha_save[[i]][it] <- cand_alpha
         M_list[[i]] <- cand_M
-        H_list[[i]] <- cand_H
-        wtime[[i]] <- cand_wtime
-
       }
       else{
         alpha_save[[i]][it] <- alpha_save[[i]][it - 1]
       }
-      wtime_save[[i]][it,] <- wtime[[i]]
+    }
+
+    #-- Update wtime
+    for(i in 1:n){
+      wtime[[i]] <- wtime_save[[i]][it,] <- as.numeric(time + create_M(x_pts = time, y_pts = feat_list[[i]], alpha = alpha_save[[i]][it]) %*%
+                                                         chol2inv(chol(create_M(feat_list[[i]], alpha = alpha_save[[i]][it])))%*%(template_feats - feat_list[[i]]))
     }
 
     #-- Update H_stack --#
+    for(i in 1:n){
+      H_list[[i]] <- splines::bs(wtime[[i]], knots = knot_loc_p, intercept = T)
+    }
     H_stack <- stack(H_list)$stack
 
     #-- Update Beta --#
@@ -135,21 +143,22 @@ template_gp_warp <- function(y_list, feat_list, template_feats,
 
     #-- Update Lam2 --#
     for(i in 1:n){
-      lam2_save[[i]][it] <- update_normal_invgamma(y = wtime[[i]], a = al, b = bl,
-                                                   mu = time, R = M_list[[i]])
+      lam2_save[[i]][it] <- update_normal_invgamma(y = template_feats, a = al, b = bl,
+                                                   mu = feat_list[[i]], R = M_list[[i]])
     }
 
   }
-  #accepts <- accepts / nrun
+  accepts <- accepts / nrun
   wtime_post <- lapply(wtime_save, function(w) apply(w[-c(1:nburn),], 2, mean))
   beta_post <- apply(beta_save[-c(1:nburn),], 2, mean)
   sig2_post <- mean(sig2_save[-c(1:nburn)])
   tau2_post <- mean(tau2_save[-c(1:nburn)])
-  #lam2_post <- lapply(lam2_save, function(w) apply(w[-c(1:nburn),], 2, mean))
-  #alpha_post <- mean(alpha_save[-c(1:nburn)])
+  lam2_post <- lapply(lam2_save, function(lvec) mean(lvec[-c(1:nburn)]))
+  alpha_post <- lapply(alpha_save, function(avec) mean(avec[-c(1:nburn)]))
 
   y_post <- lapply(1:n, function(i) interp_spline(x = wtime_post[[i]], y = y_list[[i]]))
   mean_post <- as.numeric(Hp %*% beta_post)
 
   return(list(y_post = y_post, mean_post = mean_post, accepts = accepts))
 }
+
